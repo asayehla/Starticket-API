0ef8ca15a957266562f424d733e86bc2ccbd4405
Åsa Palholmen <asa@palholmen.se>
2019-02-21 09:52:53 +0100

    Fixed updated sold tickets

--- routes/tickets.js
+++ routes/tickets.js
@@ -7,8 +7,7 @@ const Event = require('../models/event');
 //GET  Sparar den i DB
 module.exports.get = async (req, res) => {
   try {
-    let tickets = await Ticket.find({});
-    res.status(200).send(tickets);
+    res.status(200).send(await Ticket.find({}));
   } catch (err) {
     res.status(500).send(err.stack);
   }
@@ -16,29 +15,56 @@ module.exports.get = async (req, res) => {
 
 //POST
 module.exports.post = async (req, res) => {
-
   try {
+
+    //finns biljetter får min beställning plats?
+    //Uppdatera event med sålda biljetter
+
     //get event info
     let event = await Event.findById(req.body.event);
 
-    let tickets = [];
+    if (event.tickets.available >= (event.tickets.sold + req.body.amount)) {
+      //Finns biljetter kvar
+      console.info('Tickets are available!');
 
-    for (i = 0; i < req.body.amount; i++) {
+      //uppdatera event > sold ticketbo
+      let newSold = event.tickets.sold + req.body.amount;
 
-      let ticket = {
-        event: event,
-        code: uid(5),
-        used: false
+      await Event.findOneAndUpdate({
+        _id: req.body.event
+      }, {
+        tickets: {
+          sold: newSold,
+          available: event.tickets.available
+        }
+      })
+
+      //create ticket
+      let tickets = [];
+
+      for (i = 0; i < req.body.amount; i++) {
+
+        let ticket = {
+          event: event,
+          code: uid(5),
+          used: false
+        }
+
+        tickets.push(ticket);
       }
+      console.log(tickets);
 
-      tickets.push(ticket);
+      //write tickets to mongo
+      let resp = await Ticket.create(tickets);
+
+      res.status(200).send(resp);
+
+    } else {
+      // inte kvar
+      console.info('Sorry, all tickets are sold');
+      res.status(200).send('Sorry, no tickets left')
     }
-    console.log(tickets);
 
-    //write tickets to mongo
-    let resp = await Ticket.create(tickets);
-
-    res.status(200).send(resp);
   } catch (err) {
     res.status(500).send(err.stack);
   }
