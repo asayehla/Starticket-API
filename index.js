3953685741c212a7bbaef79ed49f6f2793cd6709
reeklof <reeklof@live.com>
2019-02-22 15:42:19 +0100

    removed imoprtant

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const user = 'YOUR_USER';
-const password ='bex1234';
+const password ='YOUR_PASSWORD';
 
 //connect to database
 mongoose.connect(`mongodb+srv://bex:${password}@cluster0-hl0ma.mongodb.net/test?retryWrites=true`, {
